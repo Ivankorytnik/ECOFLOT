c34f93bf0691c3662849078ee6657b00359fc96d
Ivankorytnik <koritnik2@gmail.com>
2026-09-26 08:10:04 +0300

    Analytics: connect Yandex Metrika counter 113078729

--- analytics.js
+++ analytics.js
@@ -1,5 +1,20 @@
 (function(){
-  window.ECOFLOT_METRIKA_ID = window.ECOFLOT_METRIKA_ID || null;
+  window.ECOFLOT_METRIKA_ID = 113078729;
+
+  (function(m,e,t,r,i,k,a){
+    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a);
+  })(window,document,'script','https://mc.yandex.ru/metrika/tag.js','ym');
+
+  ym(window.ECOFLOT_METRIKA_ID,'init',{
+    defer:true,
+    clickmap:true,
+    trackLinks:true,
+    accurateTrackBounce:true,
+    webvisor:true
+  });
 
   window.ecoflotGoal = function(name, params){
     try {
@@ -7,7 +22,7 @@
     } catch(e) {}
 
     try {
-      if(window.ECOFLOT_METRIKA_ID && typeof window.ym === 'function'){
+      if(typeof window.ym === 'function'){
         window.ym(window.ECOFLOT_METRIKA_ID, 'reachGoal', name, params || {});
       }
     } catch(e) {}
